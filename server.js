ad37e6908ade2a082b5e70edef657362973f26d6
paglipay <paglipay@gmail.com>
2024-08-17 19:00:38 +0000

    comment mongoose

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const port = process.env.PORT || 3001;
 
 const app = express();
@@ -9,38 +9,38 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Database is connected..."))
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log("Database is connected..."))
+//   .catch((err) => console.log(err));
 
-//db schema
-const userSchema = mongoose.Schema({
-  name: String,
-  lastName: String,
-});
+// //db schema
+// const userSchema = mongoose.Schema({
+//   name: String,
+//   lastName: String,
+// });
 
-//db model
-const User = new mongoose.model("User", userSchema);
+// //db model
+// const User = new mongoose.model("User", userSchema);
 
-app.get("/get-users", (req, res) => {
-  User.find()
-    .then((users) => res.json(users))
-    .catch((err) => console.log(err));
-});
+// app.get("/get-users", (req, res) => {
+//   User.find()
+//     .then((users) => res.json(users))
+//     .catch((err) => console.log(err));
+// });
 
-app.post("/create", (req, res) => {
-  //save to mongodb and send response
-  const newUser = new User({
-    name: req.body.name,
-    lastName: req.body.lastName,
-  });
+// app.post("/create", (req, res) => {
+//   //save to mongodb and send response
+//   const newUser = new User({
+//     name: req.body.name,
+//     lastName: req.body.lastName,
+//   });
 
-  newUser
-    .save()
-    .then((user) => res.json(user))
-    .catch((err) => console.log(err));
-});
+//   newUser
+//     .save()
+//     .then((user) => res.json(user))
+//     .catch((err) => console.log(err));
+// });
 
 //production script
 app.use(express.static("./client/build "));
